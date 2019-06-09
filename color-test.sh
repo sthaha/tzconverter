e4f4cd4d982282b65bfe957b80cc5c560c322ed4
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2019-06-10 01:44:45 +1000

    color-test

---
+++ color-test.sh
@@ -0,0 +1,19 @@
+c red hello
+c light-red hello
+c bold red hello
+c bold light-red hello
+echo
+
+c green hello
+c light-green hello
+c bold green hello
+c bold light-green hello
+echo
+
+c blue hello
+c light-blue hello
+c bold blue hello
+c bold light-blue hello
+echo
+
+
